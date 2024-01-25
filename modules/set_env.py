56ef9b87dcc16b6fc7dc7fac75955d58fa4ebf9d
LogicKahanHai (Rishi Bhalla) <contact.logickahanhai@gmail.com>
2024-01-26 00:42:57 +0530

    Troubleshooting .bashrc

--- modules/set_env.py
+++ modules/set_env.py
@@ -45,7 +45,7 @@ class SetEnv:
         bashrc.write("\nexport JAVA_HOME=" + java_home)
         bashrc.write("\nexport PATH=$PATH:$JAVA_HOME/bin")
         bashrc.close()
-        os.system(f". {home}/.bashrc")
+        print(sp.getoutput(["echo $JAVA_HOME"]))
         print(".bashrc updated...")
 
     def add_hadoop(self):
