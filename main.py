453727bc02186c7d54e2304cef59afd596a6f3af
LogicKahanHai (Rishi Bhalla) <contact.logickahanhai@gmail.com>
2024-01-28 23:25:35 +0530

    added the test function to main

--- main.py
+++ main.py
@@ -140,7 +140,8 @@ if __name__ == "__main__":
                     main.db, worker_ip, worker_dns, data_dir, hadoop_dir
                 )
                 hadoop_slave_setup.setup_slave()
-                
+                hadoop_slave_setup.check_java_slave()
+
                 
         elif choice == "4":
             print("Exiting program...")
